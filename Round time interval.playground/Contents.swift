b2d3895953b3098d147089d7233fa6019461dcd3
Boris Yurkevich <boris.yurkevich@gmail.com>
2016-01-17 11:31:39 +0000

    Refactoring

--- Round time interval.playground/Contents.swift
+++ Round time interval.playground/Contents.swift
@@ -1,3 +1,5 @@
+// Round NSTimeInterval to seconds and milliseconds double
+
 import Foundation
 
 let msIn1️⃣Sec = 1000.0 // Milliseconds in one second
@@ -8,8 +10,8 @@ let y = 3.93281102180481
 // Cutting number,
 // also possible with string formatter
 // this is not precise
-let interval = Int(y)
-let ms = Int((y % 1) * msIn1️⃣Sec)
+let intervalInSeconds = Int(y)
+let msLeft = Int((y % 1) * msIn1️⃣Sec)
 
-// Real rounding
-let real = Double(round(msIn1️⃣Sec*x) / msIn1️⃣Sec)
+// Real rounding to milliseconds
+let realMs = Double(round(msIn1️⃣Sec*x) / msIn1️⃣Sec)
