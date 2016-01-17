166f7f34a4f418bc9588396929d38b2a54b3425a
Boris Yurkevich <boris.yurkevich@gmail.com>
2016-01-17 11:29:13 +0000

    Move constant to the top

--- Round time interval.playground/Contents.swift
+++ Round time interval.playground/Contents.swift
@@ -1,14 +1,15 @@
 import Foundation
 
+let msIn1️⃣Sec = 1000.0 // Milliseconds in one second
+
 let x = 3.07314896583557
 let y = 3.93281102180481
 
 // Cutting number,
 // also possible with string formatter
-// this not precise
+// this is not precise
 let interval = Int(y)
-let ms = Int((y % 1) * 1000)
+let ms = Int((y % 1) * msIn1️⃣Sec)
 
 // Real rounding
-let msIn1️⃣Sec = 1000.0
 let real = Double(round(msIn1️⃣Sec*x) / msIn1️⃣Sec)
