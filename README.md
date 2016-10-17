08539e6a9c090774b58aaeb4db6fbdac7d659d01
Boris Yurkevich <boris.yurkevich@locassa.com>
2016-10-17 10:18:39 +0100

    Integrate all code snippets into a single playground, update to Swift 3

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Just a useful code snippets that I would like to reuse in my projects. Each action has separate Xcode playground.
 
+Updated for Swift 3.
+
 ## Actions.
 ### Round time interval.
 Round NSTimeInterval to seconds and milliseconds double.
@@ -9,3 +11,5 @@ Round NSTimeInterval to seconds and milliseconds double.
 Launch something with a delay.
 ### Tomorrow.
 Find the first date in tomorrow.
+### Alert.
+Show alert from error.
---
+++ Snippets.playground/Contents.swift
@@ -0,0 +1,56 @@
+import Foundation
+import UIKit
+
+// Tomorrow
+
+let cal = Calendar.current
+let date = Date()
+let morning = cal.startOfDay(for: date as Date)
+
+//
+// Delay Function
+// 
+// Launch something with a delay
+// Credits http://stackoverflow.com/a/29317035/1162044
+
+func delay(delay:UInt32, closure:()->()) {
+    sleep(delay)
+    closure()
+}
+
+// Example
+
+let boilingWaterSpeedInSeconds:UInt32 = 2
+print("Let's make some tea!")
+print("Turn a kettle on")
+delay(delay: boilingWaterSpeedInSeconds) {
+    print("Pour a cup ☕️")
+}
+
+//
+// Round NSTimeInterval to seconds and milliseconds double
+//
+
+let msIn1️⃣Sec = 1000.0 // Milliseconds in one second
+
+let x = 3.07314896583557
+let y = 3.93281102180481
+
+// Cutting number,
+// also possible with string formatter
+// this is not precise
+let intervalInSeconds = Int(y)
+let msLeft = Int(y.truncatingRemainder(dividingBy:1) * msIn1️⃣Sec)
+
+// Real rounding to milliseconds
+let realMs = Double(round(msIn1️⃣Sec * x) / msIn1️⃣Sec)
+
+//
+// Show Alert
+//
+
+let error = NSError(domain: "net.borisy.appname", code: 0, userInfo: nil)
+let alert = UIAlertController(title: error.localizedDescription, message: error.localizedFailureReason, preferredStyle: .alert)
+let ok = UIAlertAction(title: NSLocalizedString("OK", comment: "alert button"), style: .default, handler: nil)
+alert.addAction(ok)
+ // self.presentViewController(alert, animated: true, completion: nil)
